3b507b8e7f5e09c61b9c6d49cd9e856ce88aa0e3
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-12-07 15:23:11 +0300

    Update templateData stcruct to contain Snippets field

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/dazakharova/snippetbox-go/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
