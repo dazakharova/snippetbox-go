1effa75a0df982edfd4f77dd82ce30e325cbbfea
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-12-25 19:40:49 +0300

    Add CSRF token checks to prevent CSRF attacks

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,17 +11,17 @@ func (app *application) routes() http.Handler {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	//Unprotected routes
-	mux.Handle("GET /{$}", app.sessionManager.LoadAndSave(http.HandlerFunc(app.home)))
-	mux.Handle("GET /snippet/view/{id}", app.sessionManager.LoadAndSave(http.HandlerFunc(app.snippetView)))
-	mux.Handle("GET /user/signup", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userSignup)))
-	mux.Handle("POST /user/signup", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userSignupPost)))
-	mux.Handle("GET /user/login", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userLogin)))
-	mux.Handle("POST /user/login", app.sessionManager.LoadAndSave(http.HandlerFunc(app.userLoginPost)))
+	mux.Handle("GET /{$}", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.home))))
+	mux.Handle("GET /snippet/view/{id}", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.snippetView))))
+	mux.Handle("GET /user/signup", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.userSignup))))
+	mux.Handle("POST /user/signup", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.userSignupPost))))
+	mux.Handle("GET /user/login", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.userLogin))))
+	mux.Handle("POST /user/login", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.userLoginPost))))
 
 	//Protected routes
-	mux.Handle("GET /snippet/create", app.sessionManager.LoadAndSave(app.requireAuthentication(http.HandlerFunc(app.snippetCreate))))
-	mux.Handle("POST /snippet/create", app.sessionManager.LoadAndSave(app.requireAuthentication(http.HandlerFunc(app.snippetCreatePost))))
-	mux.Handle("POST /user/logout", app.sessionManager.LoadAndSave(app.requireAuthentication(http.HandlerFunc(app.userLogoutPost))))
+	mux.Handle("GET /snippet/create", app.sessionManager.LoadAndSave(preventCSRF(app.requireAuthentication(http.HandlerFunc(app.snippetCreate)))))
+	mux.Handle("POST /snippet/create", app.sessionManager.LoadAndSave(preventCSRF(app.requireAuthentication(http.HandlerFunc(app.snippetCreatePost)))))
+	mux.Handle("POST /user/logout", app.sessionManager.LoadAndSave(preventCSRF(app.requireAuthentication(http.HandlerFunc(app.userLogoutPost)))))
 
 	return app.recoverPanic(app.logRequest(commonHeader(mux)))
 }
