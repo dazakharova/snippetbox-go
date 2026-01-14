8ea32f69abe5e76ebbd0c6ad829d39ef23b89ed3
Daria Zakharova <daria.zakharova1404@gmail.com>
2026-01-14 19:30:47 +0200

    Add unit test for testing ping handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -30,6 +30,10 @@ type UserLoginForm struct {
 	validator.Validator `form:"-"`
 }
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 	data.Form = UserSignupForm{}
